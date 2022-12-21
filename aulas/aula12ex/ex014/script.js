8fb8d2f8541b53610119df550ffe339c4ab1837c
tryndinha0 <120797506+tryndinha0@users.noreply.github.com>
2022-12-21 18:25:04 -0300

    Update script.js

--- aulas/aula12ex/ex014/script.js
+++ aulas/aula12ex/ex014/script.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = document.querySelector('div#msg')
     var img = document.querySelector('img#imagem')
     var data = new Date()
-    var hora =  30//data.getHours()
+    var hora = 25//data.getHours()
 
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12 ){
@@ -18,4 +18,5 @@ function carregar() {
         img.src = 'noite.png'
         document.body.style.background = '#0b0b5cc5'
     }
+    
 }
